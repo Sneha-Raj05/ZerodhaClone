d609427e22102d42154a1da7fed5b6b0cfc81b8f
Sneha Raj <sneharaj211979@gmail.com>
2025-07-26 22:58:32 +0530

    Add files via upload

---
+++ Dashboard/Frontend/src/components/Holdings.js
@@ -0,0 +1,87 @@
+
+import React, { useState, useEffect, useContext } from "react";
+import axios from "axios";
+import { VerticalGraph } from "./VerticalGraph";
+import GeneralContext from "./GeneralContext"; // YE ADD KARO
+
+const Holdings = () => {
+  const [allHoldings, setAllHoldings] = useState([]);
+  
+  // Context se functions extract karo
+  const { openBuyWindow, openSellWindow } = useContext(GeneralContext);
+
+  useEffect(() => {
+    axios.get("http://localhost:3002/allHoldings").then((res) => {
+      setAllHoldings(res.data);
+    });
+  }, []);
+
+  const labels = allHoldings.map((subArray) => subArray["name"]);
+  const data = {
+    labels,
+    datasets: [
+      {
+        label: "Stock Price",
+        data: allHoldings.map((stock) => stock.price),
+        backgroundColor: "rgba(255, 99, 132, 0.5)",
+      },
+    ],
+  };
+
+    const handleBuyClick = (stock) => {
+    openBuyWindow(stock);
+  };
+
+  const handleSellClick = (stock) => {
+    openSellWindow(stock);
+  };
+
+
+  return (
+    <>
+      <h3 className="title">Holdings ({allHoldings.length})</h3>
+      <div className="order-table">
+        <table>
+          <tbody>
+            <tr>
+              <th>Instrument</th>
+              <th>Qty.</th>
+              <th>Avg. cost</th>
+              <th>LTP</th>
+              <th>Cur. val</th>
+              <th>P&L</th>
+              <th>Net chg.</th>
+              <th>Day chg.</th>
+
+            </tr>
+            {allHoldings.map((stock, index) => {
+              const curValue = stock.price * stock.qty;
+              const isProfit = curValue - stock.avg * stock.qty >= 0.0;
+              const profClass = isProfit ? "profit" : "loss";
+              const dayClass = stock.isLoss ? "loss" : "profit";
+              
+              return (
+                <tr key={index}>
+                  <td>{stock.name}</td>
+                  <td>{stock.qty}</td>
+                  <td>{stock.avg.toFixed(2)}</td>
+                  <td>{stock.price.toFixed(2)}</td>
+                  <td>{curValue.toFixed(2)}</td>
+                  <td className={profClass}>
+                    {(curValue - stock.avg * stock.qty).toFixed(2)}
+                  </td>
+                  <td className={profClass}>{stock.net}</td>
+                  <td className={dayClass} style={{fontSize: '14px'}}>{stock.day}</td>
+
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+      <VerticalGraph data={data} />
+    </>
+  );
+};
+
+export default Holdings;
